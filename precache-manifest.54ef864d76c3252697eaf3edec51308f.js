b6858285af7da9007e2663b89d913d614cfc0401
nicolasariza <nicolas_ariza@outlook.com>
2020-04-17 15:07:25 -0500

    Updates

---
+++ precache-manifest.54ef864d76c3252697eaf3edec51308f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af14bc84cd0ae43e5879a9a4135c7e87",
+    "url": "/neverlegal/index.html"
+  },
+  {
+    "revision": "539cfc249d52c0e81719",
+    "url": "/neverlegal/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d2c362e252fbc6c5ba16",
+    "url": "/neverlegal/static/css/main.53685b92.chunk.css"
+  },
+  {
+    "revision": "539cfc249d52c0e81719",
+    "url": "/neverlegal/static/js/2.8a410c6d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/neverlegal/static/js/2.8a410c6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2c362e252fbc6c5ba16",
+    "url": "/neverlegal/static/js/main.99b52f65.chunk.js"
+  },
+  {
+    "revision": "30d8fcc31ef0d5645ff1",
+    "url": "/neverlegal/static/js/runtime-main.b280e32f.js"
+  },
+  {
+    "revision": "7239fe40a0897ad27e3b9604f26a1744",
+    "url": "/neverlegal/static/media/1.7239fe40.png"
+  },
+  {
+    "revision": "1647010e8b7dd22b8398495ce850d004",
+    "url": "/neverlegal/static/media/2.1647010e.png"
+  },
+  {
+    "revision": "20d3815449b6c84de8652ec49688193b",
+    "url": "/neverlegal/static/media/3.20d38154.png"
+  },
+  {
+    "revision": "5f0a144003dd531a8acaf26551938d2d",
+    "url": "/neverlegal/static/media/4.5f0a1440.png"
+  }
+]);
